dbdd5a68b23722098e1f088239d7111baa07933f
Maib Hassin Syed <maibhassin@gmail.com>
2020-02-26 07:38:37 +0000

    add keyboard fuctionality

--- script/script.js
+++ script/script.js
@@ -1,62 +1,60 @@
 function computerPlay() {
     let computerMove = Math.ceil(Math.random() * 3);
 
-    return (computerMove == 1)? 'rock':
-           (computerMove == 2)? 'paper':
-           'scissors';
+    return (computerMove == 1) ? 'rock' :
+        (computerMove == 2) ? 'paper' :
+        'scissors';
 }
 
 function playRound(computerSelection, playerSelection) {
-        // return 1: player wins
-        // return 0: draw
-        // return -1: computer wins
-        
-        switch(playerSelection) {
-            case 'rock':
-                switch(computerSelection) {
-                    case 'rock':
-                        return [0, computerSelection, playerSelection];
-                        break;
-                    case 'paper':
-                        return [-1, computerSelection, playerSelection];
-                        break;
-                    case 'scissors':
-                        return [1, computerSelection, playerSelection];
-                        break;
-                }
-                break;
-            case 'paper':
-                switch(computerSelection) {
-                    case 'rock':
-                        return [1, computerSelection, playerSelection];
-                        break;
-                    case 'paper':
-                        return [0, computerSelection, playerSelection];
-                        break;
-                    case 'scissors':
-                        return [-1, computerSelection, playerSelection];
-                        break;
-                }
-                break;
-            case 'scissors':
-                switch(computerSelection) {
-                    case 'rock':
-                        return [-1, computerSelection, playerSelection];
-                        break;
-                    case 'paper':
-                        return [1, computerSelection, playerSelection];
-                        break;
-                    case 'scissors':
-                        return [0, computerSelection, playerSelection];
-                        break;
-                }
-                break;
-            default:
-                return [0, computerSelection, playerSelection];
-        }
-    }
+    // return 1: player wins
+    // return 0: draw
+    // return -1: computer wins
 
-document.querySelectorAll('.player__move').forEach(press => press.onclick = (e) => game(e.target.name, computerPlay()));
+    switch (playerSelection) {
+        case 'rock':
+            switch (computerSelection) {
+                case 'rock':
+                    return [0, computerSelection, playerSelection];
+                    break;
+                case 'paper':
+                    return [-1, computerSelection, playerSelection];
+                    break;
+                case 'scissors':
+                    return [1, computerSelection, playerSelection];
+                    break;
+            }
+            break;
+        case 'paper':
+            switch (computerSelection) {
+                case 'rock':
+                    return [1, computerSelection, playerSelection];
+                    break;
+                case 'paper':
+                    return [0, computerSelection, playerSelection];
+                    break;
+                case 'scissors':
+                    return [-1, computerSelection, playerSelection];
+                    break;
+            }
+            break;
+        case 'scissors':
+            switch (computerSelection) {
+                case 'rock':
+                    return [-1, computerSelection, playerSelection];
+                    break;
+                case 'paper':
+                    return [1, computerSelection, playerSelection];
+                    break;
+                case 'scissors':
+                    return [0, computerSelection, playerSelection];
+                    break;
+            }
+            break;
+        default:
+            return [0, computerSelection, playerSelection];
+    }
+}
 
 
 function game(playerSelection, computerSelection) {
@@ -64,31 +62,31 @@ function game(playerSelection, computerSelection) {
     humanMove(playerSelection, results[0]);
     computerMove(computerSelection, results[0]);
     console.log(results);
-    
 }
 
 let humanScore = 0;
 let computerScore = 0;
+
 function humanMove(move, result) {
-    
+
     const humanScoreList = document.querySelector('.human__scores');
     let imgAddress;
     let resultText;
     let resultTextColor;
-    
-    if(move == 'rock') {
+
+    if (move == 'rock') {
         imgAddress = '../images/icons8-hand-rock-100.png';
-    } else if(move == 'paper') {
+    } else if (move == 'paper') {
         imgAddress = '../images/icons8-hand-100.png';
     } else {
         imgAddress = '../images/icons8-hand-scissors-100.png';
     }
 
-    if(result == 1) {
+    if (result == 1) {
         resultText = 'You Win!';
         resultTextColor = 'green';
         humanScore++;
-    } else if(result == -1) {
+    } else if (result == -1) {
         resultText = 'You Loose!';
         resultTextColor = 'red';
     } else {
@@ -96,14 +94,8 @@ function humanMove(move, result) {
         resultTextColor = 'white';
     }
 
-    
-    humanScoreList.appendChild(updateScore(imgAddress, '#1e90ff', '#1e2dff', resultText, resultTextColor, humanScore, 'row'));
 
-    if(humanScore == 5) {
-        alert('You won');
-    } else if(computerScore == 5) {
-        alert('You Lost');
-    }
+    humanScoreList.appendChild(updateScore(imgAddress, '#1e90ff', '#1e2dff', resultText, resultTextColor, humanScore, 'row'));
 }
 
 function computerMove(move, result) {
@@ -113,18 +105,18 @@ function computerMove(move, result) {
     let resultText;
     let resultTextColor;
 
-    if(move == 'rock') {
+    if (move == 'rock') {
         imgAddress = '../images/icons8-rock-100.png';
-    } else if(move == 'paper') {
+    } else if (move == 'paper') {
         imgAddress = '../images/icons8-paper-100.png';
     } else {
         imgAddress = '../images/icons8-scissors-100.png';
     }
 
-    if(result == 1) {
+    if (result == 1) {
         resultText = 'Computer Lost';
         resultTextColor = 'red';
-    } else if(result == -1) {
+    } else if (result == -1) {
         resultText = 'Computer Won';
         resultTextColor = 'green';
         computerScore++;
@@ -135,11 +127,13 @@ function computerMove(move, result) {
 
     computerScoreList.appendChild(updateScore(imgAddress, '#ff651e', '#ff1e1e', resultText, resultTextColor, computerScore, 'row-reverse'));
 
-    if(humanScore == 5) {
-        alert('You won');
-    } else if(computerScore == 5) {
-        alert('You Lost');
-    }
+    // if(humanScore == 5) {
+    //     alert('You won');
+    //     restartGame();
+    // } else if(computerScore == 5) {
+    //     alert('You Lost');
+    //     restartGame();
+    // }
 }
 
 
@@ -152,25 +146,25 @@ function updateScore(imgAddress, ImgBgColor, imgBdColor, result, resultColor, sc
     const listItemContainer__image = document.createElement('img');
     const listItemContainer__result = document.createElement('p');
     const listItemContainer__score = document.createElement('p');
-    
+
     // content
-        //container
+    //container
     listItemContainer.style.padding = '3px';
     listItemContainer.style.display = 'flex';
     listItemContainer.style.flexDirection = flexDirection;
     listItemContainer.style.justifyContent = 'space-around';
     listItemContainer.style.alignItems = 'center';
-        // image
+    // image
     listItemContainer__image.src = imgAddress;
     listItemContainer__image.style.height = '30px';
     listItemContainer__image.style.width = '30px';
     listItemContainer__image.style.backgroundColor = ImgBgColor;
     listItemContainer__image.style.border = `2px solid ${imgBdColor}`;
-        // result
+    // result
     listItemContainer__result.textContent = result;
     listItemContainer__result.style.color = resultColor;
     listItemContainer__result.style.fontWeight = 'bold';
-        // score
+    // score
     listItemContainer__score.textContent = score;
 
     // append inside container
@@ -178,6 +172,58 @@ function updateScore(imgAddress, ImgBgColor, imgBdColor, result, resultColor, sc
     listItemContainer.appendChild(listItemContainer__result);
     listItemContainer.appendChild(listItemContainer__score);
 
-
     return listItemContainer;
 }
+
+function restartGame() {
+    const humanScoreList = document.querySelector('.human__scores');
+    const computerScoreList = document.querySelector('.computer__scores');
+    humanScoreList.innerHTML = "";
+    computerScoreList.innerHTML = ""
+    humanScore = 0;
+    computerScore = 0;
+}
+
+function restart() {
+    if (humanScore == 5) {
+        alert('you won');
+        restartGame();
+    } else if (computerScore == 5) {
+        alert('you lost');
+        restartGame();
+    }
+}
+
+document.querySelectorAll('.player__move').forEach(press => press.onclick = (e) => {
+    game(e.target.name, computerPlay());
+    restart();
+});
+
+// keyboard functionality
+
+function keyGame(e) {
+    let playerMove;
+    const key = document.querySelector(`.play__key[data-key="${e.keyCode}"]`);
+    if (!key) return;
+    key.classList.add('pressed');
+    if (e.keyCode == 65) {
+        playerMove = 'rock';
+    } else if (e.keyCode == 83) {
+        playerMove = 'paper'
+    } else {
+        playerMove = 'scissors';
+    }
+    game(playerMove, computerPlay());
+    restart();
+}
+
+
+function removeTransition(e) {
+    if (e.propertyName != 'transform') return;
+    this.classList.remove('pressed');
+}
+
+const keys = document.querySelectorAll('.play__key');
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+window.addEventListener('keydown', keyGame);
