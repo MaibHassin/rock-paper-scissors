1b68118a80df44df0469458c554df37b75770014
Maib Hassin Syed <maibhassin@gmail.com>
2020-02-26 06:49:03 +0000

    add "first to 5 win" functionality

--- script/script.js
+++ script/script.js
@@ -1,106 +1,3 @@
-
-
-// function playerPlay() {
-//     let playerMove = prompt('Please write your move (rock, paper or scissors)');
-//     return playerMove.toLowerCase();
-// }
-
-// 
-
-// function resultsTable(game_number, match_result, computer_move, player_move, computer_score, player_score) {
-//     this.game_number = game_number;
-//     this.match_result = match_result;
-//     this.computer_move = computer_move;
-//     this.player_move = player_move;
-//     this.computer_score = computer_score;
-//     this.player_score = player_score;
-// }
-
-// function game() {
-//     let computerScore = 0, playerScore = 0, gameNumber = 0;
-//     for (let i = 0; i < 5; i++) {
-//         let result = playRound(computerPlay(), playerPlay());
-//         if(result[0]>0) {
-//             playerScore++; 
-//             gameNumber++;
-//             var r = new resultsTable(gameNumber, 'Player Won', result[1], result[2], computerScore, playerScore);
-//             console.clear();
-//             console.table(r);
-//         } else if (result[0]<0) {
-//             computerScore++;
-//             gameNumber++;
-//             var r = new resultsTable(gameNumber, 'Computer Won', result[1], result[2], computerScore, playerScore);
-//             console.clear();
-//             console.table(r);
-//         } else {
-//             gameNumber++;
-//             var r = new resultsTable(gameNumber, 'Draw', result[1], result[2], computerScore, playerScore);
-//             console.clear();
-//             console.table(r);
-//         }
-//     }
-//     if(playerScore > computerScore) {
-//         console.log(`%c You WON!`, 'color: green');
-//     } else if(computerScore > playerScore) {
-//         console.log(`%c You LOOSE!`, 'color: red');
-//     } else {
-//         console.log('TIE!');
-//     }
-// }
-
-// game();
-
-// function playRound(computerSelection, playerSelection) {
-    //     // return 1: player wins
-    //     // return 0: draw
-    //     // return -1: computer wins
-    
-    //     switch(playerSelection) {
-    //         case 'rock':
-    //             switch(computerSelection) {
-    //                 case 'rock':
-    //                     return [0, computerSelection, playerSelection];
-    //                     break;
-    //                 case 'paper':
-    //                     return [-1, computerSelection, playerSelection];
-    //                     break;
-    //                 case 'scissors':
-    //                     return [1, computerSelection, playerSelection];
-    //                     break;
-    //             }
-    //             break;
-    //         case 'paper':
-    //             switch(computerSelection) {
-    //                 case 'rock':
-    //                     return [1, computerSelection, playerSelection];
-    //                     break;
-    //                 case 'paper':
-    //                     return [0, computerSelection, playerSelection];
-    //                     break;
-    //                 case 'scissors':
-    //                     return [-1, computerSelection, playerSelection];
-    //                     break;
-    //             }
-    //             break;
-    //         case 'scissors':
-    //             switch(computerSelection) {
-    //                 case 'rock':
-    //                     return [-1, computerSelection, playerSelection];
-    //                     break;
-    //                 case 'paper':
-    //                     return [1, computerSelection, playerSelection];
-    //                     break;
-    //                 case 'scissors':
-    //                     return [0, computerSelection, playerSelection];
-    //                     break;
-    //             }
-    //             break;
-    //         default:
-    //             return [0, computerSelection, playerSelection];
-    //     }
-    // }
-
-
 function computerPlay() {
     let computerMove = Math.ceil(Math.random() * 3);
 
@@ -109,18 +6,76 @@ function computerPlay() {
            'scissors';
 }
 
+function playRound(computerSelection, playerSelection) {
+        // return 1: player wins
+        // return 0: draw
+        // return -1: computer wins
+        
+        switch(playerSelection) {
+            case 'rock':
+                switch(computerSelection) {
+                    case 'rock':
+                        return [0, computerSelection, playerSelection];
+                        break;
+                    case 'paper':
+                        return [-1, computerSelection, playerSelection];
+                        break;
+                    case 'scissors':
+                        return [1, computerSelection, playerSelection];
+                        break;
+                }
+                break;
+            case 'paper':
+                switch(computerSelection) {
+                    case 'rock':
+                        return [1, computerSelection, playerSelection];
+                        break;
+                    case 'paper':
+                        return [0, computerSelection, playerSelection];
+                        break;
+                    case 'scissors':
+                        return [-1, computerSelection, playerSelection];
+                        break;
+                }
+                break;
+            case 'scissors':
+                switch(computerSelection) {
+                    case 'rock':
+                        return [-1, computerSelection, playerSelection];
+                        break;
+                    case 'paper':
+                        return [1, computerSelection, playerSelection];
+                        break;
+                    case 'scissors':
+                        return [0, computerSelection, playerSelection];
+                        break;
+                }
+                break;
+            default:
+                return [0, computerSelection, playerSelection];
+        }
+    }
+
 document.querySelectorAll('.player__move').forEach(press => press.onclick = (e) => game(e.target.name, computerPlay()));
 
+
 function game(playerSelection, computerSelection) {
-    console.log(playerSelection, computerSelection);
-    humanMove(playerSelection);
-    computerMove(computerSelection);
+    let results = playRound(computerSelection, playerSelection)
+    humanMove(playerSelection, results[0]);
+    computerMove(computerSelection, results[0]);
+    console.log(results);
+    
 }
 
-function humanMove(move) {
+let humanScore = 0;
+let computerScore = 0;
+function humanMove(move, result) {
+    
     const humanScoreList = document.querySelector('.human__scores');
     let imgAddress;
-
+    let resultText;
+    let resultTextColor;
+    
     if(move == 'rock') {
         imgAddress = '../images/icons8-hand-rock-100.png';
     } else if(move == 'paper') {
@@ -129,12 +84,34 @@ function humanMove(move) {
         imgAddress = '../images/icons8-hand-scissors-100.png';
     }
 
-    humanScoreList.appendChild(updateScore(imgAddress, '#1e90ff', '#1e2dff', 'You Won', 'green', 1, 'row'));
+    if(result == 1) {
+        resultText = 'You Win!';
+        resultTextColor = 'green';
+        humanScore++;
+    } else if(result == -1) {
+        resultText = 'You Loose!';
+        resultTextColor = 'red';
+    } else {
+        resultText = 'Draw';
+        resultTextColor = 'white';
+    }
+
+    
+    humanScoreList.appendChild(updateScore(imgAddress, '#1e90ff', '#1e2dff', resultText, resultTextColor, humanScore, 'row'));
+
+    if(humanScore == 5) {
+        alert('You won');
+    } else if(computerScore == 5) {
+        alert('You Lost');
+    }
 }
 
-function computerMove(move) {
+function computerMove(move, result) {
+
     const computerScoreList = document.querySelector('.computer__scores');
     let imgAddress;
+    let resultText;
+    let resultTextColor;
 
     if(move == 'rock') {
         imgAddress = '../images/icons8-rock-100.png';
@@ -144,7 +121,25 @@ function computerMove(move) {
         imgAddress = '../images/icons8-scissors-100.png';
     }
 
-    computerScoreList.appendChild(updateScore(imgAddress, '#ff651e', '#ff1e1e', 'You Won', 'green', 1, 'row-reverse'));
+    if(result == 1) {
+        resultText = 'Computer Lost';
+        resultTextColor = 'red';
+    } else if(result == -1) {
+        resultText = 'Computer Won';
+        resultTextColor = 'green';
+        computerScore++;
+    } else {
+        resultText = 'Draw';
+        resultTextColor = 'white';
+    }
+
+    computerScoreList.appendChild(updateScore(imgAddress, '#ff651e', '#ff1e1e', resultText, resultTextColor, computerScore, 'row-reverse'));
+
+    if(humanScore == 5) {
+        alert('You won');
+    } else if(computerScore == 5) {
+        alert('You Lost');
+    }
 }
 
 
